7ebceb0ab465abdf457cacc549dcb37d4461b036
toast <levirooth29@gmail.com>
2025-06-04 21:34:34 +0200

    awdwad

---
+++ script.js
@@ -0,0 +1,4 @@
+window.onload = () => {
+  const iframe = document.getElementById("embeddedPage");
+  iframe.src = "https://live.garmin.com/session/e162481b-5edc-800e-a915-dbc858405701/token/11A26E3BB58A3E22459C99D6B2E5E5D";
+};
